67023d164dcc070391bd7f675b62580226f237ca
Vaish <vaishm2@emc.com>
2018-10-02 17:57:53 +0530

    devops demo

--- src/index.js
+++ src/index.js
@@ -21,16 +21,38 @@ AppInsights.downloadAndSetup({ instrumentationKey: "7045e116-16b7-451b-983b-e3b9
 
 /* example: track page view */
 AppInsights.trackPageView(
-    "FirstPage", /* (optional) page name */
+    "Home Page", /* (optional) page name */
     null, /* (optional) page url if available */
     { prop1: "prop1", prop2: "prop2" }, /* (optional) dimension dictionary */
     { measurement1: 1 }, /* (optional) metric dictionary */
     123 /* page view duration in milliseconds */
 );
+AppInsights.trackPageView(
+  "Booking Page", /* (optional) page name */
+  null, /* (optional) page url if available */
+  { prop1: "prop1", prop2: "prop2" }, /* (optional) dimension dictionary */
+  { measurement1: 1 }, /* (optional) metric dictionary */
+  10 /* page view duration in milliseconds */
+);
+AppInsights.trackPageView(
+  "Location Page", /* (optional) page name */
+  null, /* (optional) page url if available */
+  { prop1: "prop1", prop2: "prop2" }, /* (optional) dimension dictionary */
+  { measurement1: 1 }, /* (optional) metric dictionary */
+  12 /* page view duration in milliseconds */
+);
 
 /* example: track event */
-AppInsights.trackEvent("TestEvent", { prop1: "prop1", prop2: "prop2" }, { measurement1: 1 });
+AppInsights.trackEvent("booking complete", { username: "prop1", city: "prop2" }, { measurement1: 4 });
+AppInsights.trackEvent("Profile Updated", { username: "prop1", city: "prop2" }, { measurement1: 5 });
+AppInsights.trackEvent("booking complete", { username: "prop1", city: "prop2" }, { measurement1: 3 });
 
+
+AppInsights.trackException("GMP vendor Api not available");
+AppInsights.trackException("User could not complete booking due to incomplete profile");
+AppInsights.trackException("Null reference exception at booking API,");
+//AppInsights.trackException("User could not complete booking due to incomplete profile","GMP.bookMyParking",{ prop1: "prop1", prop2: "prop2" },12,AI.SeverityLevel.Warning);
+//AppInsights.trackException("Null reference exception at booking API,","GMP.bookMyParking",{ prop1: "prop1", prop2: "prop2" },12,AI.SeverityLevel.Critical);
 
 const store = configureStore();
 store.dispatch(loadCourses());
